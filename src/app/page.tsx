ae7d4b02e2b1f1db7136445337c90cd9596d8532
LGY <leegyoung12@gmail.com>
2024-08-06 11:41:08 +0900

    Feat: 카카오맵 지도 출력
    - 카카오맵 api 받아와 화면에 지도 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
 import React from "react";
 import Button from "../components/Button/Button";
+import KakaoMap from "../components/Map/KakaoMap";
 
+// 'http://localhost:3000/' 경로(루트의 콘텐츠 표시
 const Home: React.FC = () => {
-    return <div></div>;
+  return (
+    <div className="flex ">
+      <main className="absolute top-0 left-0 w-screen h-screen">
+        {/* kakao 지도 호출 */}
+        <KakaoMap />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
