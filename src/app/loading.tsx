ec0fde80d43303a194e21bda89055f2adb1d8836
yerim <a123dpfla51@gmail.com>
2024-08-09 17:40:03 +0900

    Feat: Loading 페이지 생성

---
+++ src/app/loading.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+const Loading: React.FC = () => {
+    const randomNum = Math.floor(Math.random() * 7) + 1;
+
+    return (
+        <Grid
+            container
+            direction="column"
+            className="bg-main-green w-screen h-screen fixed top-0 left-0 z-50 flex justify-center items-center"
+        >
+            <img src={`dog${randomNum}.png`} width="300" height="300" className="mb-5 rounded-full" />
+            <p className="text-5xl font-bold">어디가개</p>
+        </Grid>
+    );
+};
+
+export default Loading;
