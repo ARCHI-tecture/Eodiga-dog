eb7fe3536724260c97dbcbad394242777556b8fe
LGY <leegyoung12@gmail.com>
2024-08-12 02:01:22 +0900

    create: 경도, 위도만 포함된 배열 생성
    - api data 중 경도, 위도만 포함된 배열 생성

---
+++ src/utils/db/PlaceList.tsx
@@ -0,0 +1,17 @@
+export const PlaceList = async () => {
+  const API_KEY =
+    "yAVzxpkULGQRrM5zy4zu8iDmxlYO2S0lyY%2FkF6QV996eDh3eKBzOgM0j7YZwpW%2F%2BECYL4OkhcMNsVYswLD8TwQ%3D%3D";
+
+  const data = await fetch(
+    `https://api.odcloud.kr/api/15111389/v1/uddi:41944402-8249-4e45-9e9d-a52d0a7db1cc?page=1&perPage=10&returnType=JSON&serviceKey=${API_KEY}`
+  ).then((resp) => resp.json());
+
+  // data 중 위도와 경도 값 추출
+  const placeList = data.data.map((item: any) => ({
+    lat: item["경도"],
+    log: item["위도"],
+  }));
+
+  console.log(placeList);
+  return placeList;
+};
