8a06a19557fa04d359e748bb1f7fde14ff295d65
jodasom <jodasom0216@gmail.com>
2024-08-13 14:58:20 +0900

    Feat:보러가기버튼 구현

--- src/containers/review/ReviewsList.tsx
+++ src/containers/review/ReviewsList.tsx
@@ -1,22 +1,24 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import { QueryClient, useQuery, useQueryClient } from '@tanstack/react-query';
 import { reviewRoute  } from '../../app/reviews/reviewroute';
 import Button from '../../components/Button/Button';
-import { useRouter } from 'next/navigation';
-//보러가기버튼: 리스트와 상세페이지가 완성되면 각 업체마다 특정 번호?기호?키?를 부여 route에서 주소를 만들어서 상세페이지로 이동???
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useNavigate } from 'react-router-dom';
+import { AlternateEmail } from '@mui/icons-material';
 
 interface Review {
   key: number;
   date: string;
   shopname: string;
   content: string;
+  lat:number;
+  lng:number;
 }
 
-const ReviewsList: React.FC = () => {
-  const queryClient = useQueryClient();
-  const router = useRouter();
 
+const ReviewsList: React.FC = () => {
+//데이터 상태관리
   const { data, isLoading, error } = useQuery<Review[]>({
     queryKey: ['reviews'],
     queryFn: reviewRoute ,
@@ -32,6 +34,15 @@ const ReviewsList: React.FC = () => {
   //   queryClient.invalidateQueries({queryKey:['reviews']});
   // };
 
+  //보러가기 버튼을 누르면 위도와 경도를 url에 출력및 홈으로 이동
+  const handleMoveBtn = (lng: number | null, lat: number | null) => {
+    if (lng !== null && lat !== null) {
+      window.location.href = `/?lat=${lat}&lng=${lng}`;
+    }else{
+      alert("해당 장소가 존재하지않습니다")
+    }
+};
+
   return (
     <div>
       <h1>내 리뷰</h1>
@@ -41,7 +52,11 @@ const ReviewsList: React.FC = () => {
             <div>{review.date}</div>
             <div>{review.shopname}</div>
             <div>{review.content}</div>
-            <Button variant="pink" width="w-40" border="rounded-2xl">보러가기</Button>
+            <Button
+              variant="pink" width="w-40" border="rounded-2xl"
+              onClick={() => handleMoveBtn(review.lng, review.lat)}
+              >보러가기
+            </Button>
             <Button
               //onClick={() => handleDeleteBtn(review.key)}
               variant="red" width="w-40" border="rounded-2xl">삭제하기</Button>
