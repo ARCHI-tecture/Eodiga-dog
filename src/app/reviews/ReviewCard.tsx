e10e3b126feb138cb5f1a3da91cbf876c128f5c8
LGY <leegyoung12@gmail.com>
2024-08-14 10:10:33 +0900

    Fix: 머지 후 코드 충돌 수정
    - 마이페이지 pr 머지 후 충돌 수정
    - 카드 컴포넌트 내 좌표값 전달 수정 예정

--- src/app/reviews/ReviewCard.tsx
+++ src/app/reviews/ReviewCard.tsx
@@ -2,26 +2,49 @@ import { Box, Grid } from "@mui/material";
 import React from "react";
 import Button from "../../components/Button/Button";
 
+interface Review {
+  key: number;
+  date: string;
+  shopname: string;
+  content: string;
+  lat: string;
+  lng: string;
+}
+
 const ReviewCard: React.FC<ReviewCardPropsType> = ({ date, name, review }) => {
-    // 리뷰 카드 입니다
-    // 사용 예시: <ReviewCard date="24.05.19" name="38도씨" review="정말 좋아요~" />
-    return (
-        <Grid className="m-3 max-w-max-card">
-            <Box className="border border-gray h-48 rounded-xl">
-                <Grid className="pt-5 pl-10 ">
-                    <Grid className="pl-2 mb-2">
-                        <p className="text-gray-400">{date}</p>
-                        <h3 className="font-bold text-3xl">{name}</h3>
-                        <p className="text-gray-500">{review}</p>
-                    </Grid>
-                    <Grid className="flex">
-                        <Button variant="pink">보러 가기</Button>
-                        <Button variant="red">삭제 하기</Button>
-                    </Grid>
-                </Grid>
-            </Box>
+  //보러가기 버튼을 누르면 위도와 경도를 url에 출력및 홈으로 이동
+  const handleMoveBtn = (lng: number | null, lat: number | null) => {
+    if (lng !== null && lat !== null) {
+      window.location.href = `/?lat=${lat}&lng=${lng}`;
+    } else {
+      alert("해당 장소가 존재하지않습니다");
+    }
+  };
+
+  // 리뷰 카드 입니다
+  // 사용 예시: <ReviewCard date="24.05.19" name="38도씨" review="정말 좋아요~" />
+  return (
+    <Grid className="m-3 max-w-max-card">
+      <Box className="h-48 border border-gray rounded-xl">
+        <Grid className="pt-5 pl-10 ">
+          <Grid className="pl-2 mb-2">
+            <p className="text-gray-400">{date}</p>
+            <h3 className="text-3xl font-bold">{name}</h3>
+            <p className="text-gray-500">{review}</p>
+          </Grid>
+          <Grid className="flex">
+            <Button
+              variant="pink"
+              onClick={() => handleMoveBtn(review.lng, review.lat)}
+            >
+              보러 가기
+            </Button>
+            <Button variant="red">삭제 하기</Button>
+          </Grid>
         </Grid>
-    );
+      </Box>
+    </Grid>
+  );
 };
 
 export default ReviewCard;
