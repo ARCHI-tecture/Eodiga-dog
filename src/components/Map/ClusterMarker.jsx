b9d04b1d9730aaa425be053ef3f0857677cce1ae
LGY <leegyoung12@gmail.com>
2024-08-18 22:16:44 +0900

    Fix: 클러스터 클릭 시 지도 확대
    - 클러스터 클릭 시 지도 확대되지 않는 문제 해결

--- src/components/Map/ClusterMarker.jsx
+++ src/components/Map/ClusterMarker.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { placeList } from "../../utils/db/placeList";
+import { placeList } from "../../utils/db/PlaceList";
 import { MapMarker, MarkerClusterer } from "react-kakao-maps-sdk";
 
 export const ClusterMarker = () => {
@@ -18,11 +18,13 @@ export const ClusterMarker = () => {
 
   const onClusterclick = (_target, cluster) => {
     const map = mapRef.current;
-    // 현재 지도 레벨에서 1레벨 확대한 레벨
-    const level = map.getLevel() - 1;
+    if (map) {
+      // 현재 지도 레벨에서 1레벨 확대한 레벨
+      const level = map.getLevel() - 1;
 
-    // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
-    map.setLevel(level, { anchor: cluster.getCenter() });
+      // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
+      map.setLevel(level, { anchor: cluster.getCenter() });
+    }
   };
 
   return (
@@ -32,7 +34,7 @@ export const ClusterMarker = () => {
       averageCenter={true}
       // 클러스터 할 최소 지도 레벨
       minLevel={7}
-      disableClickZoom={true}
+      disableClickZoom={false}
       // 마커 클러스터러에 클릭이벤트를 등록합니다
       // 마커 클러스터러를 생성할 때 disableClickZoom을 true로 설정하지 않은 경우
       // 이벤트 헨들러로 cluster 객체가 넘어오지 않을 수도 있습니다
@@ -48,6 +50,8 @@ export const ClusterMarker = () => {
           }}
         />
       ))}
+      {/* 마커 테스트 코드 */}
+      <MapMarker position={{ lat: 33.450701, lng: 126.570667 }} />
       {/* </> */}
     </MarkerClusterer>
   );
