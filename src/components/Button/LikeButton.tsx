10113d6518ef197d24e30a63e58d568f3ba5bc52
NOH Hyomin <117340855+Hyom01@users.noreply.github.com>
2024-09-05 10:56:48 +0900

    Update:marker 표시

--- src/components/Button/LikeButton.tsx
+++ src/components/Button/LikeButton.tsx
@@ -7,75 +7,79 @@ import { LikeButtonProps } from "./type";
 import { useQuery } from "@tanstack/react-query";
 import Bookmark from "../../containers/bookmark/bookmark";
 import { bookmarkroute } from "../../app/bookmark/bookmarkroute";
-import { useSession } from 'next-auth/react';
+import { useSession } from "next-auth/react";
 
-const LikeButton: React.FC<LikeButtonProps> = ({ liked = false, item, removeBookmark }) => {
-    const [like, setLike] = useState(liked);
-    const { data: session } = useSession();
+const LikeButton: React.FC<LikeButtonProps> = ({
+  liked = false,
+  item,
+  removeBookmark,
+}) => {
+  const [like, setLike] = useState(liked);
+  const { data: session } = useSession();
 
-    const { data } = useQuery<Bookmark[]>({
-        queryKey: ["bookmarks"],
-        queryFn: bookmarkroute,
-    });
+  const { data } = useQuery<Bookmark[]>({
+    queryKey: ["bookmarks"],
+    queryFn: bookmarkroute,
+  });
 
-    const { data: session } = useSession();
+  useEffect(() => {
+    if (data && item) {
+      const isLiked = data?.some(
+        (ele) => ele.shopname === item.시설명 || ele.id === item.id
+      );
+      setLike(!!isLiked);
+    }
+  }, [data, item]);
 
-    useEffect(() => {
-        if (data && item) {
-            const isLiked = data?.some((ele) => ele.shopname === item.시설명 || ele.id === item.id);
-            setLike(!!isLiked);
-        }
-    }, [data, item]);
+  const handleFavorite = async () => {
+    try {
+      setLike((prevLike) => !prevLike);
 
-    const handleFavorite = async () => {
-        try {
-            setLike((prevLike) => !prevLike);
+      const likedItem = {
+        id: item?.id,
+        shopname: item?.시설명,
+        location: item?.["시군구 명칭"]
+          ? item?.["시군구 명칭"]
+          : item?.["시도 명칭"],
+        type: item?.["기본 정보_장소설명"],
+        user_id: session?.user?.name ?? "unknown",
+        lat: item?.위도,
+        lng: item?.경도,
+      };
 
-            const likedItem = {
-                id: item?.id,
-                shopname: item?.시설명,
-                location: item?.["시군구 명칭"] ? item?.["시군구 명칭"] : item?.["시도 명칭"],
-                type: item?.["기본 정보_장소설명"],
-                user_id: session?.user?.name ?? 'unknown',
-                lat: item?.위도,
-                lng: item?.경도,
-            };
+      // 요청 타입 설정
+      const method = like ? "DELETE" : "POST";
 
-            // 요청 타입 설정
-            const method = like ? "DELETE" : "POST";
+      // 서버로 데이터 전송
+      const response = await fetch("/api/bookmark", {
+        method: method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(likedItem),
+      });
 
-            // 서버로 데이터 전송
-            const response = await fetch("/api/bookmark", {
-                method: method,
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(likedItem),
-            });
+      if (!response.ok) {
+        throw new Error("좋아요 처리 중 오류가 발생했습니다.");
+      }
 
-            if (!response.ok) {
-                throw new Error("좋아요 처리 중 오류가 발생했습니다.");
-            }
+      if (removeBookmark && method === "DELETE") {
+        removeBookmark(item?.id);
+      }
+    } catch (error) {
+      setLike((prevLike) => !prevLike);
+    }
+  };
 
-            if (removeBookmark && method === "DELETE") {
-                removeBookmark(item?.id);
-            }
-        } catch (error) {
-            setLike((prevLike) => !prevLike);
-        }
-    };
-
-
-
-    return (
-        <IconButton aria-label="like" onClick={handleFavorite}>
-            {like ? (
-                <FavoriteIcon className="text-main-pink" fontSize="large" />
-            ) : (
-                <FavoriteBorderIcon className="text-main-pink" fontSize="large" />
-            )}
-        </IconButton>
-    );
+  return (
+    <IconButton aria-label="like" onClick={handleFavorite}>
+      {like ? (
+        <FavoriteIcon className="text-main-pink" fontSize="large" />
+      ) : (
+        <FavoriteBorderIcon className="text-main-pink" fontSize="large" />
+      )}
+    </IconButton>
+  );
 };
 
 export default LikeButton;
