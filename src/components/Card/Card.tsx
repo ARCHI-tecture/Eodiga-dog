ce18cc5e73467273eb5ea1cf9e94251fbb8ab2ec
LGY <leegyoung12@gmail.com>
2024-09-06 14:24:30 +0900

    Feat: 커서 모양 변경
    카드에 마우스 오버 시 커서 모양 pointer로 변경

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,28 +4,28 @@ import LikeButton from "../Button/LikeButton";
 import { CardPropsType } from "./type";
 
 const Card: React.FC<CardPropsType> = ({ item }) => {
-    // 사이드바 전용 카드라 크기가 전체화면으로 설정되어 있습니다
+  // 사이드바 전용 카드라 크기가 전체화면으로 설정되어 있습니다
 
-    return (
-        <Grid className="flex justify-center m-3">
-            <Box className="border border-gray w-full h-56 rounded-xl flex flex-col justify-center">
-                <Grid className="flex items-center justify-around">
-                <h3 className="font-bold text-2xl">{item.시설명}</h3>
-                    <LikeButton item={item} />
-                </Grid>
-                <Grid className="flex flex-col">
-                <p className="whitespace-pre-wrap pl-10 pr-4">
-                        {item.도로명주소 ? item.도로명주소 : item.지번주소}
-                    </p>
-                    <Grid className="pl-10">
-                    <p>{item.운영시간}</p>
-                        <span>⭐️ 4.3 / </span>
-                        <span>리뷰 999+</span>
-                    </Grid>
-                </Grid>
-            </Box>
+  return (
+    <Grid className="flex justify-center m-3">
+      <Box className="flex flex-col justify-center w-full h-56 border cursor-pointer border-gray rounded-xl">
+        <Grid className="flex items-center justify-around">
+          <h3 className="text-2xl font-bold">{item.시설명}</h3>
+          <LikeButton item={item} />
         </Grid>
-    );
+        <Grid className="flex flex-col">
+          <p className="pl-10 pr-4 whitespace-pre-wrap">
+            {item.도로명주소 ? item.도로명주소 : item.지번주소}
+          </p>
+          <Grid className="pl-10">
+            <p>{item.운영시간}</p>
+            <span>⭐️ 4.3 / </span>
+            <span>리뷰 999+</span>
+          </Grid>
+        </Grid>
+      </Box>
+    </Grid>
+  );
 };
 
 export default Card;
