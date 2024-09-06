f49424b603203ca98e7ad0b754e52c8b34e72e32
LGY <leegyoung12@gmail.com>
2024-09-06 20:49:04 +0900

    Fix: 버튼 css 개선
    - 마우스 오버 시 css 엇나가는 현상 개선

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -5,53 +5,55 @@ import PersonIcon from "@mui/icons-material/Person";
 import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react";
 
-
 const Header: React.FC = () => {
+  const { data: session } = useSession();
 
-    const { data: session } = useSession();
-    
-    console.log(session)
-    const buttonStyle =
-        "text-main-black text-md font-bold hover:border hover:border-black hover:rounded-3xl pl-5 pr-5 pt-2 pb-2 ";
+  console.log(session);
+  const buttonStyle =
+    "text-main-black text-md font-bold border-2 border-transparent hover:border-black hover:rounded-3xl pl-5 pr-5 pt-2 pb-2";
 
-
-    return (
+  return (
+    <div className="flex">
+      <div className="fixed z-40 flex items-center justify-between w-full h-16 p-10 bg-main-green">
+        <Link href="/">
+          <h1 className="text-xl text-main-black">어디가개</h1>
+        </Link>
         <div className="flex">
-            <div className="bg-main-green fixed w-full h-16 z-40 flex justify-between items-center p-10">
-                <Link href="/">
-                    <h1 className="text-main-black text-xl">어디가개</h1>
-                </Link>
-                <div className="flex">
-                    {session ? (
-                        <>
-                            <button className="text-main-black text-md font-bold mr-12">OO님</button>
-                            <Link href="/mypage">
-                                <button className="text-main-black text-sm font-bold flex flex-col items-center">
-                                    <PersonIcon />
-                                    마이페이지
-                                </button>
-                            </Link>
-                            {/* 임시 로그아웃 버튼 */}
-                            <button                    
-                                onClick={() => {
-                                    signOut()
-                                }}>임시 로그아웃</button>
-                        </>
-                    ) : (
-                        <>
-                            <Link href="/login">
-                                <button className={`${buttonStyle} mr-5`}>로그인</button>
-                            </Link>
-                            <Link href="/signup">
-                                <button className={buttonStyle}>회원가입</button>
-                            </Link>
-                        </>
-                    )}
-                </div>
-            </div>
-            <div className="pb-20"></div> {/* 헤더 높이만큼 메인에 여백  */}
+          {session ? (
+            <>
+              <button className="mr-12 font-bold text-main-black text-md">
+                OO님
+              </button>
+              <Link href="/mypage">
+                <button className="flex flex-col items-center text-sm font-bold text-main-black">
+                  <PersonIcon />
+                  마이페이지
+                </button>
+              </Link>
+              {/* 임시 로그아웃 버튼 */}
+              <button
+                onClick={() => {
+                  signOut();
+                }}
+              >
+                임시 로그아웃
+              </button>
+            </>
+          ) : (
+            <>
+              <Link href="/login">
+                <button className={`${buttonStyle} mr-5`}>로그인</button>
+              </Link>
+              <Link href="/signup">
+                <button className={buttonStyle}>회원가입</button>
+              </Link>
+            </>
+          )}
         </div>
-    );
+      </div>
+      <div className="pb-20"></div> {/* 헤더 높이만큼 메인에 여백  */}
+    </div>
+  );
 };
 
 export default Header;
